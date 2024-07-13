51209b99abea5907f9141f78177d945c95926c62
doppelzwei <minisalami04@gmail.com>
2024-07-13 20:14:00 +0200

    Remove unused reference

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 // Author: Leander Kroth
 
 // References:
-// https://iss.oden.utexas.edu/Publications/Papers/burtscher11.pdf
 // https://learnopengl.com/
 // https://bashbaug.github.io/OpenCL-Docs/pdf/OpenCL_API.pdf
 // https://web.engr.oregonstate.edu/~mjb/cs575/Handouts/opencl.opengl.vbo.1pp.pdf
@@ -31,6 +30,5 @@ int main(int, char**)
 
         glwrapper::SwapBuffers();
     }
-
     return 0;
 }
